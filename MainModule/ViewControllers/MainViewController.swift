b9d88933a3c687e40751a3a00ca0fa0c02d250ed
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-27 23:10:50 +0500

    UITableViewDataSource register cell

--- MainModule/ViewControllers/MainViewController.swift
+++ MainModule/ViewControllers/MainViewController.swift
@@ -7,14 +7,32 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+final class MainViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    private func setupViews() {
         title = "Просмотр"
         view.backgroundColor = .white
     }
+}
 
-
+// MARK: UITableViewDataSource
+extension MainViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
+    }
 }
 
