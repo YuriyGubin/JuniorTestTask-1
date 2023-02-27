b39c04dcc4dac0bce3a1cb4e1a6e64c5138b70c4
YuriyGubin <iosdevelop.2022@gmail.com>
2023-02-27 22:40:25 +0500

    Initial Commit

---
+++ JuniorTestTask#1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JuniorTestTask#1
+//
+//  Created by Yuriy on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
